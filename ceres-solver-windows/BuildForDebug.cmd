c3066825702d04f412f40a879749ba0ccd826193
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-07-14 15:34:27 +0900

    Add new-line symbols to cmake calling commands

--- ceres-solver-windows/BuildForDebug.cmd
+++ ceres-solver-windows/BuildForDebug.cmd
@@ -23,7 +23,12 @@ cd ..
 
 
 cd ceres-solver_build_x64
-cmake.exe ../ceres-solver -G "Visual Studio 14 2015 Win64" -DCMAKE_SUPPRESS_REGENERATION:BOOL=TRUE -DEIGENSPARSE:BOOL=TRUE -DEIGEN_INCLUDE_DIR=../eigen-eigen-5a0156e40feb -DGFLAGS:BOOL=TRUE -DGFLAGS_INCLUDE_DIR=../gflags_build_x64/include -DGFLAGS_LIBRARY=../gflags_build_x64/lib/Debug/gflags_static.lib
+cmake.exe ../ceres-solver -G "Visual Studio 14 2015 Win64" -DCMAKE_SUPPRESS_REGENERATION:BOOL=TRUE ^
+-DEIGENSPARSE:BOOL=TRUE ^
+-DEIGEN_INCLUDE_DIR=../eigen-eigen-5a0156e40feb ^
+-DGFLAGS:BOOL=TRUE ^
+-DGFLAGS_INCLUDE_DIR=../gflags_build_x64/include ^
+-DGFLAGS_LIBRARY=../gflags_build_x64/lib/Debug/gflags_static.lib
 cmake.exe --build "." --target "ALL_BUILD" --config "Debug"
 cd ..
 
