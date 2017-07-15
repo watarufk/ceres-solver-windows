4bccaa97d3548088b841663540ec6cb371cf002e
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-07-15 15:01:45 +0900

    Update Eigen3.cmd

--- ceres-solver-windows/Eigen3.cmd
+++ ceres-solver-windows/Eigen3.cmd
@@ -12,26 +12,26 @@ cd eigen-eigen-5a0156e40feb_build_x64
 
 REM eigen is header library.  BUILD_TESTING must be TRUE.
 cmake.exe ../eigen-eigen-5a0156e40feb -G "Visual Studio 14 2015 Win64" ^
--DCMAKE_CONFIGURATION_TYPES:STRING="Release" ^
+-DCMAKE_CONFIGURATION_TYPES:STRING="Debug;Release" ^
 -DCMAKE_SUPPRESS_REGENERATION:BOOL=TRUE ^
 -DBUILD_TESTING:BOOL="1"
 
 echo ^
--DCMAKE_CONFIGURATION_TYPES:STRING="Debug" ^
 -DEIGEN_CUDA_COMPUTE_ARCH:STRING="30" ^
 -DEIGEN_TEST_CUDA:BOOL="1" ^
--DEIGEN_TEST_OPENMP:BOOL="1" ^
--DBoost_INCLUDE_DIR:PATH="C:\SDKs\boost_1_64_0" ^
 -DGLUT_INCLUDE_DIR:PATH="C:\SDKs\OpenGL\include" ^
 -DGLUT_glut_LIBRARY:FILEPATH="C:\SDKs\OpenGL\lib\x64\freeglut.lib" ^
 -DGLEW_INCLUDE_DIR:PATH="C:\SDKs\OpenGL\include" ^
 -DGLEW_GLEW_LIBRARY:FILEPATH="C:\SDKs\OpenGL\lib\x64\glew64.lib" ^
+-DEIGEN_TEST_OPENMP:BOOL="1" ^
+-DBoost_INCLUDE_DIR:PATH="C:\SDKs\boost_1_64_0" ^
  
 
 
 
 msbuild Eigen3.sln /m
-msbuild RUN_TESTS.vcxproj
+cmake.exe --build "." --target "RUN_TESTS" --config "Release" >> RUN_TESTS_Release_result.txt
+cmake.exe --build "." --target "RUN_TESTS" --config "Debug" >> RUN_TESTS_Debug_result.txt
 
 
 
