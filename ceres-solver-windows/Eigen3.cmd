49e0c99e1632813c31daec10380706e3ce063e9a
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-07-17 20:22:33 +0900

    Rename Eigen3_cuda_openmp.cmd to Eigen3.cmd and update it to install Eigen3 to "C:/SDKs/eigen/eigen3"

--- ceres-solver-windows/Eigen3.cmd
+++ ceres-solver-windows/Eigen3.cmd
@@ -3,7 +3,12 @@ echo on
 
 
 
-del "Eigen3_basic_*.txt"
+REM If you use MKL, set the install folder path (ex. "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl") to environment variable "MKLROOT".
+REM About details, please refer to ../eigen-eigen-5a0156e40feb/cmake/FindBLAS.cmake
+
+
+
+del "Eigen3_*.txt"
 cd ..
 rmdir /S /Q eigen-eigen-5a0156e40feb_build_x64
 mkdir eigen-eigen-5a0156e40feb_build_x64
@@ -11,29 +16,28 @@ cd eigen-eigen-5a0156e40feb_build_x64
 
 
 
-REM eigen is header library.  BUILD_TESTING must be TRUE.
+REM eigen is header library.  If BUILD_TESTING it not TRUE, cmake does not generate the project files.
 cmake.exe ../eigen-eigen-5a0156e40feb -G "Visual Studio 14 2015 Win64" ^
--DCMAKE_CONFIGURATION_TYPES:STRING="Debug;Release" ^
--DBUILD_TESTING:BOOL="1" >> "..\ceres-solver-windows\Eigen3_basic_cmake.txt" 2>&1
+-DEIGEN_TEST_CUDA:BOOL=TRUE ^
+-DEIGEN_TEST_OPENMP:BOOL=TRUE ^
+-DBoost_INCLUDE_DIR:PATH="C:\SDKs\boost_1_64_0" ^
+-DBUILD_TESTING:BOOL=TRUE ^
+-DCMAKE_CONFIGURATION_TYPES:STRING="Release" ^
+-DCMAKE_INSTALL_PREFIX:PATH="C:/SDKs/eigen/eigen3" >> "..\ceres-solver-windows\Eigen3_cmake.txt" 2>&1
 
 echo ^
 -DEIGEN_CUDA_COMPUTE_ARCH:STRING="30" ^
--DEIGEN_TEST_CUDA:BOOL="1" ^
 -DGLUT_INCLUDE_DIR:PATH="C:\SDKs\OpenGL\include" ^
 -DGLUT_glut_LIBRARY:FILEPATH="C:\SDKs\OpenGL\lib\x64\freeglut.lib" ^
 -DGLEW_INCLUDE_DIR:PATH="C:\SDKs\OpenGL\include" ^
 -DGLEW_GLEW_LIBRARY:FILEPATH="C:\SDKs\OpenGL\lib\x64\glew64.lib" ^
--DEIGEN_TEST_OPENMP:BOOL="1" ^
--DBoost_INCLUDE_DIR:PATH="C:\SDKs\boost_1_64_0" ^
  
 
+REM msbuild Eigen3.sln /m >> "..\ceres-solver-windows\Eigen3_msbuild.txt" 2>&1
+REM cmake.exe --build "." --target "RUN_TESTS" --config "Release" >> "..\ceres-solver-windows\Eigen3_RunTestsRelease.txt" 2>&1
 
-
-msbuild Eigen3.sln /m >> "..\ceres-solver-windows\Eigen3_basic_msbuild.txt" 2>&1
-
-cmake.exe --build "." --target "RUN_TESTS" --config "Release" >> "..\ceres-solver-windows\Eigen3_basic_RunTestsRelease.txt" 2>&1
-cmake.exe --build "." --target "RUN_TESTS" --config "Debug" >> "..\ceres-solver-windows\Eigen3_basica_RunTestsDebug.txt" 2>&1
+cmake.exe --build "." --target "INSTALL" --config "Release" >> "..\ceres-solver-windows\Eigen3_Install.txt" 2>&1
 
 
 
-cd ..\ceres-solver-windows
+cd /d "%~dp0"
